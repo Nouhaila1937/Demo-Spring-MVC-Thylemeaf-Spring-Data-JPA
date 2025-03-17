613c676a18545c363334f317ae8bdfd1d213a53f
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-03-17 09:35:13 +0000

    Initial commit

---
+++ src/main/java/org/example/mvcthylemeafhopital/MvcThylemeafHopitalApplication.java
@@ -0,0 +1,13 @@
+package org.example.mvcthylemeafhopital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcThylemeafHopitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcThylemeafHopitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/mvcthylemeafhopital/MvcThylemeafHopitalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mvcthylemeafhopital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcThylemeafHopitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
