23a1f84169aa3fcb61929d6482994bc081bf625a
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-04-13 01:43:59 +0100

    jdbc Auth avec vérification d'existence d'user

--- src/main/java/org/example/mvcthylemeafhopital/MvcThylemeafHopitalApplication.java
+++ src/main/java/org/example/mvcthylemeafhopital/MvcThylemeafHopitalApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -37,11 +38,20 @@ public class MvcThylemeafHopitalApplication implements CommandLineRunner {
 
     }
     @Bean
-    CommandLineRunner commandLineRunner(PatientRepository patientRepository, JdbcUserDetailsManager jdbcUserDetailsManager) {
+    CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager){
         return args -> {
-            jdbcUserDetailsManager.createUser(User.withUsername("user1").password(passwordEncoder().encode("1234")).roles("USER").build());
-            jdbcUserDetailsManager.createUser(User.withUsername("user2").password(passwordEncoder().encode("1234")).roles("USER").build());
-            jdbcUserDetailsManager.createUser(User.withUsername("admin1").password(passwordEncoder().encode("1234")).roles("USER","ADMIN").build());
+            jdbcUserDetailsManager.createUser(User.withUsername("user11").password(passwordEncoder().encode("1234")).roles("USER").build());
+            jdbcUserDetailsManager.createUser(User.withUsername("user22").password(passwordEncoder().encode("1234")).roles("USER").build());
+            jdbcUserDetailsManager.createUser(User.withUsername("admin2").password(passwordEncoder().encode("1234")).roles("USER","ADMIN").build());
+            UserDetails u1=jdbcUserDetailsManager.loadUserByUsername("user11");
+            if(u1==null)
+                jdbcUserDetailsManager.createUser(User.withUsername("user11").password(passwordEncoder().encode("1234")).roles("USER").build());
+            UserDetails u2=jdbcUserDetailsManager.loadUserByUsername("user22");
+            if(u2==null)
+                jdbcUserDetailsManager.createUser(User.withUsername("user22").password(passwordEncoder().encode("1234")).roles("USER").build());
+            UserDetails u3=jdbcUserDetailsManager.loadUserByUsername("admin2");
+            if(u3==null)
+                jdbcUserDetailsManager.createUser(User.withUsername("admin2").password(passwordEncoder().encode("1234")).roles("USER","ADMIN").build());
         };
     }
     @Bean
